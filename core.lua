e282ec9ceae6dde622971b3476c95113dc6ac87c
HolleVonBlut <hellxas1472@gmail.com>
2026-04-01 09:58:37 -0500

    Update core.lua

--- core.lua
+++ core.lua
@@ -15,8 +15,8 @@ function RM.Msg(text, r, g, b)
     end
 end
 
-RM.VERSION      = "1.4"
-RM.VERSION_NUM  = 8
+RM.VERSION      = "1.41"
+RM.VERSION_NUM  = 9
 RM.ADDON_PREFIX = "RaidMark"
 
 RM.state = {
